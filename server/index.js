cd4991d7f3f87f6cc7dffed657b96804b9ab4743
Jatin-GI <guptajatin0416@gmail.com>
2024-08-29 18:36:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3001;
 const databaseURL = process.env.DATABASE_URL;
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://react-chat-app-frontend-vc69.onrender.com');
+  res.header('Access-Control-Allow-Origin', process.env.ORIGIN);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
